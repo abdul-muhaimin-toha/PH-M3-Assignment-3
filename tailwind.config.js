be3c7daebf3c5163eb70eac07449facb16850747
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-02-13 19:29:40 +0600

    enable  alwayslight mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  plugins: [require("daisyui")],
-  darkMode: 'class',
+  content: ['./*.html'],
+  plugins: [require('daisyui')],
   theme: {
     fontFamily: {
-      sans: ["Manrope", "sans-serif"],
+      sans: ['Manrope', 'sans-serif'],
     },
     extend: {
       colors: {
-        black: "#131318",
-        primary: "#FF4240",
-        "body-dark": "rgba(255, 255, 255, 0.60)",
-        "body-light": "rgba(19, 19, 24, 0.60)",
+        black: '#131318',
+        primary: '#FF4240',
+        'body-dark': 'rgba(255, 255, 255, 0.60)',
+        'body-light': 'rgba(19, 19, 24, 0.60)',
       },
     },
   },
